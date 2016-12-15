c32b4ea97f286d78aa55b08b53dc2433831d19cb
Hyeongguk Jeon <paypoay91@hanyang.ac.kr>
2016-12-15 06:50:32 -0800

    first commit

---
+++ my_project/.build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jeon/catkin_ws/src/my_project/.build/devel:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="/home/jeon/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jeon/catkin_ws/src/my_project/.build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jeon/catkin_ws/src/my_project:$ROS_PACKAGE_PATH"
---
+++ my_project/src/my_project.cpp
@@ -0,0 +1,5 @@
+// my_project.cpp
+int main( int argc, char **argv )
+{
+  return 0;
+}
